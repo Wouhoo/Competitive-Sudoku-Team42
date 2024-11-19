9e6001117b130c36beb7a97e0e13a8a58fb2891c
Wouter <wouter@familieleenen.nl>
2024-11-19 14:58:24 +0100

    check_legal_moves v0.9

--- team42_A0/check_legal_moves.py
+++ team42_A0/check_legal_moves.py
@@ -1,5 +1,54 @@
-from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove
+from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove, Square
 
 def check_legal_moves(game_state: GameState) -> list[Move]:
+    '''
+    Checks which moves are legal for the given game state and return them as a list of Moves
+    '''
+    N = game_state.board.N
+    allowed_squares = GameState.player_squares(game_state)
+    legal_moves = []
+    # if allowed_squares is None: then every square is an allowed square
     
-    return [] # a list of Moves (Move class objects)
+    # Iterate over all possible moves ((square, value) pairs)
+    for square in allowed_squares:
+        for value in range(1, N+1):
+            # Check if move is taboo
+            if TabooMove(square, value) in game_state.taboo_moves:
+                continue
+            # Check if number already exists in row, column or region
+            if value in get_row(game_state.board, square):
+                continue
+            if value in get_column(game_state.board, square):
+                continue
+            if value in get_region(game_state.board, square):
+                continue
+            # Otherwise, add move to legal moves
+            legal_moves.append(Move(square, value))
+        
+    return legal_moves
+
+def get_full_board(game_state: GameState) -> list[Square]:
+    # Returns all squares on the board
+    return []
+
+def get_row(board: SudokuBoard, square: Square) -> list[int]:
+    # Returns all numbers present in the row that square is in
+    return [board.get((square[0], j)) for j in range(board.N)]
+
+def get_column(board: SudokuBoard, square: Square) -> list[int]:
+    # Returns all numbers present in the column that square is in
+    return [board.get((i, square[1])) for i in range(board.N)]
+
+def get_region(board: SudokuBoard, square: Square) -> list[int]:
+    # Returns all numbers present in the region that square is in
+    return [board.get((square[0]//board.m + i, square[1]//board.n + j)) for j in range(board.n) for i in range(board.m)]
+
+### DEBUG ###
+from competitive_sudoku.sudoku import parse_game_state
+import os
+
+path = os.path.join(os.getcwd(), r"..\\boards\\empty-2x3.txt")
+board_file = open(path, "r")
+test_state = parse_game_state(board_file.read(), playmode="classic")
+
+print(get_row(test_state.board, (2,3)))
