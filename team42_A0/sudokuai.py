ecd43e2819f9a49c68478968bb4df3b2a3148498
Richard <r.c.wellens@student.tue.nl>
2024-11-19 15:59:49 +0100

    Implement basic minimax

--- team42_A0/sudokuai.py
+++ team42_A0/sudokuai.py
@@ -2,35 +2,91 @@
 #  Software License, (See accompanying file LICENSE or copy at
 #  https://www.gnu.org/licenses/gpl-3.0.txt)
 
+import copy
 import random
-import time
-from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove
+
 import competitive_sudoku.sudokuai
+from competitive_sudoku.sudoku import GameState, Move, SudokuBoard
+from .check_legal_moves import check_legal_moves, get_row, get_column, get_region
+from .evaluation import evaluate_state
 
 
 class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
     """
     Sudoku AI that computes a move for a given sudoku configuration.
     """
-
     def __init__(self):
         super().__init__()
 
-    # N.B. This is a very naive implementation.
     def compute_best_move(self, game_state: GameState) -> None:
-        N = game_state.board.N
+        legal_moves = check_legal_moves(game_state)
+        # Make sure we have an initial valid move, otherwise we lose the game.
+        self.propose_move(random.choice(legal_moves))
 
-        # Check whether a cell is empty, a value in that cell is not taboo, and that cell is allowed
-        def possible(i, j, value):
-            return game_state.board.get((i, j)) == SudokuBoard.empty \
-                   and not TabooMove((i, j), value) in game_state.taboo_moves \
-                       and (i, j) in game_state.player_squares()
+        scores = {}
+        for i, move in enumerate(legal_moves):
+            new_state = _make_move(game_state, move)
+            # TODO Currently hardcodes search depth to 1, implement iterative deepening.
+            score = _minimax(new_state, depth=0, is_maximizing=True)
+            scores[i] = score
 
-        all_moves = [Move((i, j), value) for i in range(N) for j in range(N)
-                     for value in range(1, N+1) if possible(i, j, value)]
-        move = random.choice(all_moves)
-        self.propose_move(move)
-        while True:
-            time.sleep(0.2)
-            self.propose_move(random.choice(all_moves))
+        # Sort indices for legal_moves by score in decreasing order
+        best_move_idx = sorted(scores, key=scores.get, reverse=True)[0]
+        self.propose_move(legal_moves[best_move_idx])
 
+
+# Naive minimax evaluation function for a given search depth.
+def _minimax(game_state: GameState, depth: int, is_maximizing: bool):
+    """Implementation of the minimax scoring function.
+
+    Args:
+        game_state: state of the game to score.
+        depth: depth of the state tree to evaluate (>= 0).
+        is_maximizing: whether the evaluation is for the maximizing player.
+    """
+    legal_moves = check_legal_moves(game_state)
+    if depth == 0 or len(legal_moves) == 0:
+        return evaluate_state(game_state)
+
+    value, value_func = (-1e9, max) if is_maximizing else (1e9, min)
+    for move in legal_moves:
+        new_state = _make_move(game_state, move)
+        value = value_func(value, _minimax(new_state, depth - 1, not is_maximizing))
+    return value
+
+
+def _make_move(game_state: GameState, move: Move) -> GameState:
+    """Create a new game state that happens after the given move was performed.
+    Taboo moves are not added, the sudoku is assumed to remain solvable after any move.
+
+    Args:
+        game_state: State of the game before the move.
+        move: The move to make. Move is assumed to be valid.
+    """
+    new_state = copy.deepcopy(game_state)
+    new_state.board.put(move.square, move.value)
+    new_state.moves.append(move)
+
+    move_score = _move_score(new_state.board, move.square)
+    new_state.scores[new_state.current_player - 1] += move_score
+
+    if new_state.current_player == 1:
+        new_state.occupied_squares1 += [move.square]
+    else:
+        new_state.occupied_squares2 += [move.square]
+
+    new_state.current_player = 3 - new_state.current_player
+    return new_state
+
+
+def _move_score(board: SudokuBoard, square: tuple[int, int]) -> int:
+    """Compute the score for the most recent move 'square' and a given board."""
+    score = [0, 1, 3, 7]
+    n_filled = 0
+    for get_area in (get_row, get_column, get_region):
+        area = get_area(board, square)
+        # Area is filled if there are no empty cells.
+        is_filled = 0 not in area
+        n_filled += is_filled
+
+    return score[n_filled]
