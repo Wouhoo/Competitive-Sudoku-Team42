62c01d1639d65c1ec4e442e4248021239dcc646d
Wouter <wouter@familieleenen.nl>
2024-11-19 12:43:40 +0100

    Update check_legal_moves.py

---
+++ team42_A0/check_legal_moves.py
@@ -0,0 +1,4 @@
+from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove
+
+def check_legal_moves(gameState: GameState) -> list[Move]:
+    return [] # a list of Moves (Move class objects)
