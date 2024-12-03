8d1d906803c29521fce2f4c9c584a6b3bf8b37ba
Wouter <wouter@familieleenen.nl>
2024-12-03 12:21:14 +0100

    T H E   W A L L v1
    Added competitive "wall" heuristic. Try it out for yourself and leave your observations & suggestions in the comments at the bottom!

--- team42_A2/competitive_heuristics.py
+++ team42_A2/competitive_heuristics.py
@@ -1,1 +1,94 @@
 ### Flow: use get_legal_moves to get a list of moves, then filter it here ###
+from competitive_sudoku.sudoku import GameState, Move
+
+def wall_heuristic(moves: list[Move], game_state: GameState) -> list[Move]:
+    '''
+    Filters a list of legal moves based on a heuristic that attempts to "cut off" the opponent.
+    Currently this is implemented by first making a vertical line from the middle column,
+    and then making a horizontal wall midway up the board.
+    '''
+    board_middle = game_state.board.N // 2
+    
+    if(game_state.current_player == 1):
+        # Get the row number of the row closest to the middle where our player occupies a square
+        occupied_height = max([square[0] for square in game_state.occupied_squares1]) if len(game_state.occupied_squares1) > 0 else 0
+        
+        # We must first work our way towards the middle of the board; this is most efficiently done with a vertical line.
+        if(occupied_height < board_middle):
+            # Therefore, allow only moves that play in the middle column
+            filtered_moves = [move for move in moves if move.square[1] == board_middle]
+
+        # If no more moves can be played in the middle column, or the desired height (middle row) is reached, 
+        # switch to building   t h e   w a l l
+        if(occupied_height >= board_middle or len(filtered_moves) == 0):
+            # Now allow only moves that play in this row, prioritizing moves to the right
+            # (We want to finish one half of the wall before starting the other half, so we at least cut off 1/4 of the board)
+            height = min(occupied_height, board_middle)
+            filtered_moves = [move for move in moves if (move.square[0] == height and move.square[1] > board_middle)]
+            if(len(filtered_moves) == 0):
+                filtered_moves = [move for move in moves if move.square[0] == height]
+
+        # If we *also* cannot play anymore moves on the middle row, then we've completed the wall (or the enemy broke through)
+        # In this case, prioritize playing moves on the enemy's half of the board to rob them of even more turns
+        if(len(filtered_moves) == 0):
+            filtered_moves = [move for move in moves if move.square[0] > height]
+
+    # If we're player two, the process is similar, except all height comparisons are reversed
+    else:
+        # Get the row number of the row closest to the middle where our player occupies a square
+        occupied_height = min([square[0] for square in game_state.occupied_squares2]) if len(game_state.occupied_squares2) > 0 else game_state.board.N
+        
+        # We must first work our way towards the middle of the board; this is most efficiently done with a vertical line.
+        if(occupied_height > board_middle):
+            # Therefore, allow only moves that play in the middle column
+            filtered_moves = [move for move in moves if move.square[1] == board_middle]
+
+        # If no more moves can be played in the middle column, or the desired height (middle row) is reached, 
+        # switch to building   t h e   w a l l
+        if(occupied_height <= board_middle or len(filtered_moves) == 0):
+            # Now allow only moves that play in this row, prioritizing moves to the right
+            # (We want to finish one half of the wall before starting the other half, so we at least cut off 1/4 of the board)
+            height = max(occupied_height, board_middle)
+            filtered_moves = [move for move in moves if (move.square[0] == height and move.square[1] > board_middle)]
+            if(len(filtered_moves) == 0):
+                filtered_moves = [move for move in moves if move.square[0] == height]
+
+        # If we *also* cannot play anymore moves on the middle row, then we've completed the wall (or the enemy broke through)
+        # In this case, prioritize playing moves on the enemy's half of the board to rob them of even more turns
+        if(len(filtered_moves) == 0):
+            filtered_moves = [move for move in moves if move.square[0] < height]
+
+    # Finally, as a failsafe, if this filtering leaves us with no moves, then don't filter
+    if len(filtered_moves) == 0:
+        filtered_moves = moves
+
+    return filtered_moves
+
+###################
+#
+# Observations, thoughts & ideas for future upgrades
+#
+# - Instead of always rushing to the middle through the middle column, we could make this variable;
+#   you could also take a column closer to the edge so that you can more easily capture the part between 
+#   the vertical line and the edge (though as a trade-off, cutting off the other half will be harder)
+# - Instead of always prioritizing building the wall to the right, we could somehow evaluate the game state
+#   to see if building left first would be better.
+# - Currently the heuristic just assumes that the wall was succesful if no more moves can be played in the middle row.
+#   However, we *do* know if the enemy has broken through, because we know if they occupy a square on our side of the board.
+#   We should be able to use this somehow; if they have broken through, then instead of playing on their side,
+#   try to block them as much as possible on our side (but in a smart way).
+#   Not sure exactly how we want to do this though; the easy thing to do would be to just switch back to minimax
+#   if you notice the opponent has broken through, but idk how well that would work (will test next time)
+# - After finishing the wall, the heuristic is currently set up to play only on the opponent's half of the board.
+#   However, this is not necessarily better, since it might allow the opponent to complete regions using our plays; instead, 
+#   we might want to be able to "skip turns" by playing in our captured area, so that we can complete the regions instead.
+# - I'm noticing in testing that there is a relatively high risk of proposing a taboo move while building out the wall.
+#   Maybe the sudoku heuristics can help prevent this?
+# - Also in testing, this strategy seems to be more effective and consistent the larger the board is.
+#   (by more consistent I mean that it is less likely that the enemy interferes with the wall.
+#    Of course, this would be a different story if the enemy used a similar strategy)
+# - When running two A2 agents against eachother, player 1 tends to win (they get to claim one more row),
+#   *except* if a move of theirs gets declared taboo and they skip a turn; in that case player 2 can break through p1's wall.
+# - <Leave your own ideas here>
+#
+###################
--- team42_A2/sudokuai.py
+++ team42_A2/sudokuai.py
@@ -10,6 +10,7 @@ import competitive_sudoku.sudokuai
 from competitive_sudoku.sudoku import GameState, Move, SudokuBoard
 from .check_legal_moves import get_legal_moves, get_row, get_column, get_block
 from .evaluation import evaluate_state
+from .competitive_heuristics import wall_heuristic
 
 
 class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
@@ -20,9 +21,9 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         super().__init__()
 
     def compute_best_move(self, game_state: GameState) -> None:
-        legal_moves = get_legal_moves(game_state)
+        good_moves = wall_heuristic(get_legal_moves(game_state), game_state)
         # Make sure we have an initial valid move, otherwise we lose the game.
-        self.propose_move(random.choice(legal_moves))
+        self.propose_move(random.choice(good_moves))
 
         current_depth = 1
         while True:
@@ -39,20 +40,20 @@ def _find_best_move(game_state: GameState, depth: int) -> Move:
     Returns:
           The move with the highest score, or a random move if all moves are equally good.
     """
-    legal_moves = get_legal_moves(game_state)
+    good_moves = wall_heuristic(get_legal_moves(game_state), game_state)
 
     is_maximizing = game_state.current_player == 1
     scores = {}
-    for i, move in enumerate(legal_moves):
+    for i, move in enumerate(good_moves):
         new_state = _make_move(game_state, move)
         score = _minimax_alphabeta(new_state, depth=depth - 1, is_maximizing=not is_maximizing)
         scores[i] = score
 
-    # Sort indices for legal_moves by score in decreasing order
+    # Sort indices for good_moves by score in decreasing order
     ranked_move_idxs = sorted(scores, key=scores.get, reverse=True)
     best_score = scores[ranked_move_idxs[0]] if is_maximizing else scores[ranked_move_idxs[-1]]
     best_moves = [move_idx for move_idx, score in scores.items() if score == best_score]
-    return legal_moves[random.choice(best_moves)]
+    return good_moves[random.choice(best_moves)]
 
 
 def _minimax_alphabeta(
@@ -70,13 +71,13 @@ def _minimax_alphabeta(
     Returns:
         Score for the given state.
     """
-    legal_moves = get_legal_moves(game_state)
-    if depth == 0 or len(legal_moves) == 0:
+    good_moves = wall_heuristic(get_legal_moves(game_state), game_state)
+    if depth == 0 or len(good_moves) == 0:
         return evaluate_state(game_state)
 
     if is_maximizing:
         value = -math.inf
-        for move in legal_moves:
+        for move in good_moves:
             new_state = _make_move(game_state, move)
             value = max(value, _minimax_alphabeta(new_state, depth - 1, alpha, beta, not is_maximizing))
             alpha = max(alpha, value)
@@ -85,7 +86,7 @@ def _minimax_alphabeta(
         return value
     else:
         value = math.inf
-        for move in legal_moves:
+        for move in good_moves:
             new_state = _make_move(game_state, move)
             value = min(value, _minimax_alphabeta(new_state, depth - 1, alpha, beta, not is_maximizing))
             beta = min(beta, value)
