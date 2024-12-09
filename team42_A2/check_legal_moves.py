febd11ff510aae33a47f2209744ea0d4542648c5
Richard <r.c.wellens@student.tue.nl>
2024-12-10 00:04:37 +0100

    Add sudoku heuristics

--- team42_A2/check_legal_moves.py
+++ team42_A2/check_legal_moves.py
@@ -1,51 +1,63 @@
 from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, TabooMove, Square
 
+
 def get_legal_moves(game_state: GameState) -> list[Move]:
     """
     Checks which moves are legal for the given game state, and return them as a list of Moves.
     """
     N = game_state.board.N
     allowed_squares = GameState.player_squares(game_state)
-    if allowed_squares is None:   # if allowed_squares is None, then every square is an allowed square
-        allowed_squares = full_board(game_state.board)
+    # if allowed_squares is None, then every square is an allowed square
+    if allowed_squares is None:
+        allowed_squares = full_board(game_state)
+
     legal_moves = []
-    
+    taboo_moves = [(move.square, move.value) for move in game_state.taboo_moves]
     # Iterate over all possible moves ((square, value) pairs)
     for square in allowed_squares:
-        for value in range(1, N+1):
-            # Check if move is taboo
-            if TabooMove(square, value) in game_state.taboo_moves:
-                continue
-            # Check if number already exists in row, column or region
-            if value in get_row(game_state.board, square):
-                continue
-            if value in get_column(game_state.board, square):
-                continue
-            if value in get_block(game_state.board, square):
-                continue
-            # If not, add move to legal moves
-            legal_moves.append(Move(square, value))
-        
+        symbols = set(range(1, N + 1))
+        symbols -= get_row(game_state.board, square)
+        symbols -= get_column(game_state.board, square)
+        symbols -= get_block(game_state.board, square)
+
+        for value in symbols:
+            if (square, value) not in taboo_moves:
+                legal_moves.append(Move(square, value))
+
     return legal_moves
 
+
 def full_board(game_state: GameState) -> list[Square]:
-    '''Returns all unoccupied squares on the board'''
-    empty_board = [(i,j) for i in range(game_state.board.N) for j in range(game_state.board.N)]
+    """Returns all unoccupied squares on the board"""
+    empty_board = [
+        (i, j) for i in range(game_state.board.N) for j in range(game_state.board.N)
+    ]
     occupied_squares = set(game_state.occupied_squares1 + game_state.occupied_squares2)
     return [square for square in empty_board if square not in occupied_squares]
 
-def get_row(board: SudokuBoard, square: Square) -> list[int]:
-    '''Returns all numbers present in the row that square is in'''
-    return [board.get((square[0], j)) for j in range(board.N)]
 
-def get_column(board: SudokuBoard, square: Square) -> list[int]:
-    '''Returns all numbers present in the column that square is in'''
-    return [board.get((i, square[1])) for i in range(board.N)]
+def get_row(board: SudokuBoard, square: Square) -> set[int]:
+    """Returns all numbers present in the row that square is in"""
+    return set(board.squares[square[0] * board.N : (square[0] + 1) * board.N])
 
-def get_block(board: SudokuBoard, square: Square) -> list[int]:
-    '''Returns all numbers present in the region that square is in'''
-    region_row, region_col = square[0]//board.m * board.m, square[1]//board.n * board.n
-    return [board.get((region_row + i, region_col + j)) for j in range(board.n) for i in range(board.m)]
+
+def get_column(board: SudokuBoard, square: Square) -> set[int]:
+    """Returns all numbers present in the column that square is in"""
+    return set(board.squares[square[1] :: board.N])
+
+
+def get_block(board: SudokuBoard, square: Square) -> set[int]:
+    """Returns all numbers present in the region that square is in"""
+    region_row, region_col = (
+        square[0] // board.m * board.m,
+        square[1] // board.n * board.n,
+    )
+    return {
+        board.get((region_row + i, region_col + j))
+        for j in range(board.n)
+        for i in range(board.m)
+    }
+
 
 ### DEBUG ###
 # from competitive_sudoku.sudoku import parse_game_state
---
+++ team42_A2/sudoku_heuristics.py
@@ -0,0 +1,199 @@
+import copy
+from functools import lru_cache, reduce
+from itertools import combinations
+
+from competitive_sudoku.sudoku import GameState, Move, SudokuBoard, Square
+from .check_legal_moves import get_row, get_column, get_block
+
+
+def sudoku_heuristics(moves: list[Move], game_state: GameState) -> list[Move]:
+    """Filter out moves that make the sudoku unsolvable.
+
+    Args:
+        moves: list of considered moves.
+        game_state: current state of the game.
+
+    Returns:
+        A new list of moves such that new_list is a subset of 'moves'.
+    """
+    moves_dict = {}  # square -> {values}
+    for move in moves:
+        if move.square not in moves_dict:
+            moves_dict[move.square] = {move.value}
+        else:
+            moves_dict[move.square].add(move.value)
+
+    new_moves = subgroup_exclusion(moves_dict, game_state.board)
+    # hidden_pair and naked_pair did not perform well on the smallest board size.
+    if game_state.board.N > 4:
+        new_moves = hidden_twin(new_moves, game_state.board, group_size=2)
+        new_moves = naked_twin(new_moves, game_state.board, group_size=2)
+
+    # Convert the resulting moves dict back into a list of moves.
+    out = []
+    for square, values in new_moves.items():
+        for value in values:
+            out.append(Move(square, value))
+
+    return out
+
+
+def subgroup_exclusion(moves: dict, board: SudokuBoard) -> dict:
+    """Applies the subgroup exclusion rule from SudokuDragon.
+
+    Args:
+        moves: dictionary of suggested moves (square -> set of suggested values).
+        board: current board
+
+    Returns:
+        An updated set of moves which has needless options removed. Remains the
+        same as 'moves' if no options were excluded.
+    """
+
+    def apply_rule(squares, values: set[int], board: SudokuBoard) -> set[int]:
+        available = set()
+        for square in squares:
+            if board.get(square) == board.empty:
+                available.update(_square_valid_moves(square, board))
+
+        if values - available:
+            return values - available
+        else:
+            return values
+
+    out = copy.deepcopy(moves)
+    for square, considered_vals in moves.items():
+        values = _square_valid_moves(square, board)
+        if len(values) <= 1:
+            continue
+
+        groups = [
+            _get_row_squares(square[0], board) - {square},
+            _get_col_squares(square[1], board) - {square},
+            _get_region_squares(square, board) - {square},
+        ]
+        for group in groups:
+            found_vals = apply_rule(group, values, board)
+            if len(found_vals) == 1:
+                out[square] = considered_vals.intersection(found_vals)
+                break
+
+    return out
+
+
+def hidden_twin(moves: dict, board: SudokuBoard, group_size=2) -> dict:
+    """Applies the hidden-twin rule from SudokuDragon.
+
+    Args:
+        moves: dictionary of suggested moves (square -> set of suggested values).
+        board: current board.
+        group_size: size of the group to look for using this method. Default 2 for twins.
+
+    Returns:
+        An updated set of moves which has needless options removed. Remains the
+        same as 'moves' if no options were excluded.
+    """
+
+    def apply_rule(squares, values, board):
+        group_moves = [_square_valid_moves(square, board) for square in squares]
+        for subset in combinations(values, group_size):
+            subset = set(subset)
+            if sum([subset.issubset(val) for val in group_moves]) == group_size - 1:
+                if (
+                    sum([subset.intersection(val) != set() for val in group_moves])
+                    == group_size - 1
+                ):
+                    return subset
+
+        return values
+
+    out = copy.deepcopy(moves)
+    for square, considered_vals in moves.items():
+        values = _square_valid_moves(square, board)
+        if len(values) <= group_size:
+            continue
+
+        groups = [
+            _get_row_squares(square[0], board) - {square},
+            _get_col_squares(square[1], board) - {square},
+            _get_region_squares(square, board) - {square},
+        ]
+        for group in groups:
+            group = {square for square in group if board.get(square) == board.empty}
+            found_vals = apply_rule(group, values, board)
+            if len(found_vals) == group_size:
+                out[square] = considered_vals.intersection(found_vals)
+                break
+
+    return out
+
+
+def naked_twin(moves: dict, board: SudokuBoard, group_size=2) -> dict:
+    """Applies the hidden-twin rule from SudokuDragon.
+
+    Args:
+        moves: dictionary of suggested moves (square -> set of suggested values).
+        board: current board.
+        group_size: size of the group to look for using this method. Default 2 for twins.
+
+    Returns:
+        An updated set of moves which has needless options removed. Remains the
+        same as 'moves' if no options were excluded."""
+
+    def apply_rule(squares, values, board):
+        group_moves = [_square_valid_moves(square, board) for square in squares]
+        available = reduce(lambda x, y: x.union(y), group_moves, set())
+
+        for subset in combinations(available, group_size):
+            subset = set(subset)
+            # Pairs without any considered moves do not give us information.
+            if len(values.intersection(subset)) == 0:
+                continue
+
+            if sum([subset == val for val in group_moves]) == group_size:
+                return values - subset
+
+        return values
+
+    out = copy.deepcopy(moves)
+    for square, values in moves.items():
+        if len(values) <= group_size:
+            continue
+
+        groups = [
+            _get_row_squares(square[0], board) - {square},
+            _get_col_squares(square[1], board) - {square},
+            _get_region_squares(square, board) - {square},
+        ]
+        for group in groups:
+            group = {square for square in group if board.get(square) == board.empty}
+            found_vals = apply_rule(group, values, board)
+            if found_vals != out[square]:
+                out[square] = found_vals
+                break
+
+    return out
+
+
+@lru_cache
+def _square_valid_moves(square: Square, board: SudokuBoard) -> set[int]:
+    """Find all numbers that can be played on this square. Assumes the square is empty."""
+    valid_numbers = set(range(1, board.N + 1))
+    valid_numbers -= get_row(board, square)
+    valid_numbers -= get_column(board, square)
+    valid_numbers -= get_block(board, square)
+    return valid_numbers
+
+
+def _get_row_squares(row_idx, board) -> set[Square]:
+    return {(row_idx, i) for i in range(board.N)}
+
+
+def _get_col_squares(col_idx, board) -> set[Square]:
+    return {(i, col_idx) for i in range(board.N)}
+
+
+def _get_region_squares(square, board) -> set[Square]:
+    x = square[0] // board.n * board.n
+    y = square[1] // board.m * board.m
+    return {(x + i, y + j) for i in range(board.n) for j in range(board.m)}
